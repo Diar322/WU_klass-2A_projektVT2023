b89e848202b7834bd195e381efd5f28546d6e9ce
Diar322 <68732081+Diar322@users.noreply.github.com>
2023-03-06 14:53:09 +0100

    Överförde allt från test till main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-const burger = document.getElementById("burger")
-const nav = document.getElementById("nav_list")
 
-function toggleMenu() {
-    burger.classList.toggle('burger_toggle')
-    nav.classList.toggle('nav-active')
+/*
+
+addEventListener("resize", (event) => {});
+
+var slider = document.getElementById("myRange");
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+
+  var elements = document.getElementsByClassName("top-nav");
+    console.log(elements)
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.paddingLeft = this.value + "px";
+        elements[i].style.paddingRight = this.value + "px";
+    }
+
+
 }
 
-burger.addEventListener('click', toggleMenu)
+onresize = (event) => {
+    let width = window.innerWidth;
+    console.log (width)
+    var elements = document.getElementsByClassName("top-nav");
+    console.log(elements)
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.paddingLeft = width/3 + "px";
+        elements[i].style.paddingRight = width/3+ "px";
+    }
+
+   
+};
+
+*/
