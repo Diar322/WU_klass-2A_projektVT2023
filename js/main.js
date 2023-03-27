1899d6e3ae1de687cfdcf9c89e02476a142f2dbf
Diar322 <68732081+Diar322@users.noreply.github.com>
2023-03-27 14:55:04 +0200

    Gjorde mer på om oss.
    Renade koden, la till photshoppade bilder. fixade med knapparna så de är eventlisteners

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ let swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
+});
+
+// knappar
+
+const collection = document.getElementsByClassName("konsert_button");
+
+for (let i = 0; i < collection.length - 1; i++) {
+  collection[i].addEventListener("click", function() {
+    window.location.href='konserter.html';
   });
+}
+
+document.getElementById("index_om_oss_button").addEventListener("click", function() {
+  window.location.href='om_oss.html';
+});;
+
